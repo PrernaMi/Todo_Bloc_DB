4642dd7414611dac269171bf0047b3ab75751d5e
Prerna <prernamittal440@gmail.com>
2024-09-15 18:40:02 +0530

    Added feature: Dark and light theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:path/path.dart';
 import 'package:todo_bloc_db/bloc/todo_bloc.dart';
 import 'package:todo_bloc_db/database/local/db_helper.dart';
+import 'package:todo_bloc_db/provider/theme_provider.dart';
 import 'package:todo_bloc_db/screens/home_screen.dart';
 import 'package:todo_bloc_db/splash/login_page.dart';
 import 'package:todo_bloc_db/splash/splash_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp( BlocProvider(create: (context){
-    return TodoBloc(mainDb: DbHelper.getInstances);
-  },child: MyApp(),));
+  runApp( MultiBlocListener(listeners: [
+    BlocProvider(create: (context){
+      return TodoBloc(mainDb: DbHelper.getInstances);
+    }),
+    ChangeNotifierProvider(create: (context){
+      return ThemeProvider();
+    })
+  ], child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
